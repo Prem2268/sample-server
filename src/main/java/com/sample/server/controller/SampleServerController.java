dc3281daec7b7d9180944a1da3b59f38f8836499
Prem2268 <veligaram.premkumar@gmail.com>
2020-03-28 22:28:45 +0530

    logger and lombock changes

--- src/main/java/com/sample/server/controller/SampleServerController.java
+++ src/main/java/com/sample/server/controller/SampleServerController.java
@@ -3,12 +3,16 @@ package com.sample.server.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/sampleserver")
 public class SampleServerController {
 	
 	@RequestMapping("/serverdata")
 	public String clientServer() {
+		log.info("Server data invoked..");
 		return "Data from Server";
 	}
 
